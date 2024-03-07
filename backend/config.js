85e7a264b3cbe27091da13ec150611d7a1c55a3b
Sivani L R <109839211+sivani-l-r@users.noreply.github.com>
2024-03-07 12:01:10 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://root:root@dsa-tracker.jt1bzkk.mongodb.net/dsa_questions_collection?retryWrites=true&w=majority';
+export const mongoDBURL = 'MONGODBURL';
