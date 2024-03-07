476027901023e3572da772cd52caba1c6e8a0773
Sivani <sivanilr03@gmail.com>
2024-03-07 10:08:12 +0530

    updated ui

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const containerStyle = {
-    maxWidth: '800px',
-    margin: '0 auto',
-    textAlign: 'center',
-  };
-  const profileImageStyle = {
-    width: '200px',
-    height: '200px',
-    borderRadius: '50%',
-    objectFit: 'cover',
-    marginBottom: '20px',
-    margin: '0 auto', // Center the profile image horizontally
-  };
+  maxWidth: '800px',
+  margin: '0 auto',
+  textAlign: 'center',
+};
+
+const profileImageStyle = {
+  width: '200px',
+  height: '200px',
+  borderRadius: '50%',
+  objectFit: 'cover',
+  marginBottom: '20px',
+  margin: '0 auto', // Center the profile image horizontally
+};
 
 const badgeContainerStyle = {
   display: 'grid',
@@ -38,25 +39,26 @@ const Profile = () => {
           <img src="src/images/profile.jpeg" alt="Profile Image" style={profileImageStyle} />
         </div>
         <h2>Name</h2>
-        
+
         <h2 style={{ textAlign: 'center' }}>My Badges</h2>
         <div style={badgeContainerStyle}>
-          {Array.from({ length: 7 }, (_, i) => (
-            <div className="circle" key={i} style={badgeStyle}>
-              <img src={`src/images/s${i + 1}.jpeg`} alt={`Badge ${i + 1}`} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-              <p className="image-text">Badge {i + 1}</p>
-            </div>
-          ))}
+          <div className="circle" style={badgeStyle}>
+            <img src="src/images/newbie.jpeg" alt="Badge 1" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+            <p className="image-text">Badge 1</p>
+          </div>
         </div>
-        
+
         <h2 style={{ textAlign: 'center', marginTop: '40px' }}>Badges to Earn</h2>
         <div style={badgeContainerStyle}>
-          {Array.from({ length: 7 }, (_, i) => (
-            <div className="circle" key={i} style={badgeStyle}>
-              <img src={`src/images/s${i + 8}.jpeg`} alt={`Badge ${i + 8}`} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-              <p className="image-text">Badge {i + 8}</p>
-            </div>
-          ))}
+          {Array.from({ length: 13 }, (_, i) => {
+            if (i === 0) return null; // Skip the first badge (newbie.jpeg)
+            return (
+              <div className="circle" key={i} style={badgeStyle}>
+                <img src={`src/images/s${i + 1}.jpeg`} alt={`Badge ${i + 1}`} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                <p className="image-text">Badge {i + 1}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
